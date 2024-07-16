c4dcb7025038e13e95328e7422ade6c5752738f7
gZanda <gabrielfzanda@gmail.com>
2024-07-16 19:55:38 -0300

    Kubectl

--- kubernetes.sh
+++ kubernetes.sh
@@ -12,3 +12,8 @@ kind create cluster --name my-cluster
 kubectl cluster-info
 
 # Install Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl cluster-info
